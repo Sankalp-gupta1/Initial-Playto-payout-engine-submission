74d8c9c02b3d91adb1f58ecfa4efa1e7ac46be3c
Sankalp Gupta <saritamit2004@gmail.com>
2026-04-28 10:28:01 +0530

    Initial Playto payout engine submission

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-3c+fx&brt!_2nm=w%q+ks06j!!8hlk((1$__@823n4@&bg@ofd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
